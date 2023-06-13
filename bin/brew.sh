f07136b0ee2d90fc315b3307f3f293382b6dbf11
Ian Wahbe <ian@wahbe.com>
2023-06-13 17:06:36 +0200

    Add global .gitignore

---
+++ bin/brew.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+brew install emacs-plus@29     \
+     --with-no-frame-refocus   \
+     --with-imagemagick        \
+     --with-native-comp        \
+     --with-poll               \
+     --with-memeplex-wide-icon
